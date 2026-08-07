3093f3a42d3bf2d2c59a6b1d007260089c8c5658
favourigwe <chinoigwe04@gmail.com>
2026-08-07 16:39:07 -0500

    Update init.py

--- src/language_modes/init.py
+++ src/language_modes/init.py
@@ -2,13 +2,12 @@
 Language models package for NewsBot Intelligence System 2.0.
 """
 
-from .summarizer import IntelligentSummarizer, ContentEnhancer
 from .embeddings import SemanticSearchEngine
 from .generator import ContentGenerator
+from .summarizer import IntelligentSummarizer
 
 __all__ = [
-    "IntelligentSummarizer",
-    "ContentEnhancer",
     "SemanticSearchEngine",
     "ContentGenerator",
+    "IntelligentSummarizer",
 ]
