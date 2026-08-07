ad2cf5668a2f6fdadcebd3677d26339ca78b6c63
favourigwe <chinoigwe04@gmail.com>
2026-08-07 15:28:12 -0500

    Update ner_extractor.py

--- src/analysis/ner_extractor.py
+++ src/analysis/ner_extractor.py
@@ -1,1 +1,14 @@
+"""
+Named Entity Recognition (NER) and relationship extraction module
+for NewsBot Intelligence System 2.0.
+"""
 
+from typing import Any, Dict, List, Tuple, Union
+
+import networkx as nx
+import plotly.graph_objects as go
+import spacy
+
+
+class EntityRelationshipMapper:
+    ...
