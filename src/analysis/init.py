290ed7f5b43b26ac6d404b8cff0f66be725db22b
favourigwe <chinoigwe04@gmail.com>
2026-08-07 15:26:01 -0500

    Update init.py

--- src/analysis/init.py
+++ src/analysis/init.py
@@ -1,1 +1,15 @@
+"""
+Analysis package for NewsBot Intelligence System 2.0.
+"""
 
+from .classifier import AdvancedNewsClassifier
+from .sentiment_analyzer import SentimentEvolutionTracker
+from .ner_extractor import EntityRelationshipMapper
+from .topic_modeler import TopicDiscoveryEngine
+
+__all__ = [
+    "AdvancedNewsClassifier",
+    "SentimentEvolutionTracker",
+    "EntityRelationshipMapper",
+    "TopicDiscoveryEngine",
+]
