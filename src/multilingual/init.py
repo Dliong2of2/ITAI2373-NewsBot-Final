a3051e602af4ff89fc0502f58139a181bf2c53af
favourigwe <chinoigwe04@gmail.com>
2026-08-07 16:40:43 -0500

    Update init.py

--- src/multilingual/init.py
+++ src/multilingual/init.py
@@ -1,1 +1,13 @@
+"""
+Multilingual processing package for NewsBot Intelligence System 2.0.
+"""
 
+from .language_detector import LanguageDetector
+from .translator import Translator
+from .cross_lingual_analyzer import CrossLingualAnalyzer
+
+__all__ = [
+    "LanguageDetector",
+    "Translator",
+    "CrossLingualAnalyzer",
+]
